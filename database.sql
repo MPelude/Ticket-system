7cbc695489737bca477fa1ab09add65de721a445
Maija <maija.pelude@inbox.lv>
2020-12-17 21:31:55 +0200

    pievienoju databazi

---
+++ database.sql
@@ -0,0 +1,33 @@
+drop database if exists ticketSystem;
+CREATE DATABASE ticketSystem;
+USE ticketSystem;
+
+CREATE TABLE tickets (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name`  VARCHAR(45) NOT NULL,
+  title VARCHAR(45) NOT NULL,
+  descript VARCHAR(45) NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` ENUM('New', 'In processing', 'Resolved') NOT NULL DEFAULT 'New'
+);
+
+CREATE TABLE actions (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  ticketID INT UNSIGNED NOT NULL,
+  FOREIGN KEY (ticketID) REFERENCES tickets (id),
+  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+   );
+   
+   select * from tickets;
+   select * from actions;
+   insert into tickets (`name`,title, descript, `status`)
+   values ('Jānis', 'test', 'test', 'New');
+    select * from tickets;
+    
+    insert into actions (ticketID)
+   values ('1');
+     select * from actions;
+    
+   
+  
+  
