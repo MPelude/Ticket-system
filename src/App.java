947432bc13ce714f90f7e426adc4fecb3573028b
Maija <maija.pelude@inbox.lv>
2020-12-22 20:39:25 +0200

    Full code

--- src/App.java
+++ src/App.java
@@ -15,40 +15,95 @@ public class App {
             System.out.println("3 - Change ticket status");
             System.out.println("4 - Weekly SLA report");
             System.out.println("5 - Exit");
-            int option = scanner.nextInt();
+            int option = Integer.parseInt(scanner.nextLine(), 10);
 
-            switch (option) {
-                case 1:
-                    break;
-                case 2:
+            if (option == 1) {
+                System.out.println("Please enter your name and press<enter>: ");
+                String name = scanner.nextLine();
 
-                    ResultSet resultSet = statement.executeQuery(
-                            "SELECT id, `name`, title, descript, status" + " FROM tickets" + " ORDER BY id;");
-                    System.out.println();
-                    System.out.println("Ticket list: ");
-                    while (resultSet.next()) {
-                        int id = resultSet.getInt("id");
-                        String name = resultSet.getString("name");
-                        String title = resultSet.getString("title");
-                        String descript = resultSet.getString("descript");
-                        String status = resultSet.getString("status");
+                System.out.print("Please enter ticket title and press<enter>: ");
+                String title = scanner.nextLine();
 
-                        System.out.println(
-                                id + " '" + name + "' - " + title + " -  " + descript + " - Status: " + status);
-                    }
-                    System.out.println();
-                    break;
+                System.out.print("Please enter ticket description and press<enter>: ");
+                String descript = scanner.nextLine();
 
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
+                System.out.print("Please enter ticket status and press<enter>: ");
+                String status = scanner.nextLine();
+
+                System.out.println("Name: " + name);
+                System.out.println("Title: " + title);
+                System.out.println("Description: " + descript);
+                System.out.println("Status: " + status);
+
+                statement.executeUpdate("INSERT INTO tickets (name, title, descript, status) " + "VALUES ('" + name
+                        + "', '" + title + "', '" + descript + "', '" + status + "')");
+
+            } else if (option == 2) {
+
+                ResultSet resultSet = statement
+                        .executeQuery("SELECT id, `name`, title, descript, status" + " FROM tickets" + " ORDER BY id;");
+                System.out.println();
+                System.out.println("Ticket list: ");
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    String title = resultSet.getString("title");
+                    String descript = resultSet.getString("descript");
+                    String status = resultSet.getString("status");
+
+                    System.out.println(id + " '" + name + "' - " + title + " -  " + descript + " - Status: " + status);
+                }
+                System.out.println();
+            } else if (option == 3) {
+
+                ResultSet resultSet1 = statement
+                        .executeQuery("SELECT id, `name`, title, descript, status" + " FROM tickets" + " ORDER BY id;");
+                System.out.println();
+                System.out.println("Ticket list: ");
+                while (resultSet1.next()) {
+                    int id = resultSet1.getInt("id");
+                    String name = resultSet1.getString("name");
+                    String title = resultSet1.getString("title");
+                    String descript = resultSet1.getString("descript");
+                    String status = resultSet1.getString("status");
+
+                    System.out.println(id + " '" + name + "' - " + title + " -  " + descript + " - Status: " + status);
+                }
+                System.out.println();
+
+                System.out.print("Please enter ticket ID : ");
+                int id = Integer.parseInt(scanner.nextLine(), 10);
+                resultSet1 = statement
+                        .executeQuery("SELECT `name`, title, descript, status" + " FROM tickets" + " WHERE id = " + id);
+                System.out.println();
+                System.out.println("Ticket: ");
+                while (resultSet1.next()) {
+                    String name = resultSet1.getString("name");
+                    String title = resultSet1.getString("title");
+                    String descript = resultSet1.getString("descript");
+                    String status = resultSet1.getString("status");
+
+                    System.out.println(id + " '" + name + "' - " + title + " -  " + descript + " - Status: " + status);
+                }
+                System.out.println();
+
+                System.out.print("Please choose new status  : ");
+                String status = scanner.nextLine();
+
+                if (status == "Resolved") {
+                    statement.executeUpdate("UPDATE tickets SET status = '" + status
+                            + "', resolution = CURRENT_TIMESTAMP() WHERE id = " + id);
+                } else {
+                    statement.executeUpdate("UPDATE tickets SET status = '" + status + "' WHERE id = " + id);
+                }
+
+            } else if (option == 4) {
+            } else if (option == 5) {
 
             }
         } catch (Exception e) {
             System.out.println("Something went wrong.");
+            System.out.println(e.getMessage());
         }
     }
 }
